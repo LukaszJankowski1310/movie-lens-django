c7be68d50886e90727655f74ee67f1aad5777e62
teraz_kubi <kubacichowicz13@wp.pl>
2023-05-25 00:34:48 +0200

    models

--- movielens/userview/admin.py
+++ movielens/userview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Rating, Genre, Movie, Comment, Image
 
 # Register your models here.
+admin.site.register(Rating)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Comment)
+admin.site.register(Image)
--- movielens/userview/models.py
+++ movielens/userview/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
 class Genre(models.Model):
     name = models.CharField(max_length=300)
     def __str__(self):
         return self.name
 
-
-# tmp to authorization
 class Comment(models.Model):
     text = models.CharField(max_length=2000)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    # movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     date = models.DateField()
     def __str__(self):
         return str(self.author) + "; " + str(self.movie.title) + "; " + str(self.text)
 
+class Image(models.Model):
+    #sciezka do pliku
+    path = models.CharField(max_length=200)
+    isFrontImage = models.BooleanField()
 
 class Movie(models.Model):
     title = models.CharField(max_length=1000)
+
     genres = models.ManyToManyField(Genre)
-
-    imdb_ref = None
-    image = None
-
     comments = models.ManyToManyField(Comment)
-
+    images = models.ManyToManyField(Image)
+    
+    #IMDB cross reference
+    imdb_ref = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.title+";    (" + "".join([str(genre)+" " for genre in self.genres.all()]) + ")"
