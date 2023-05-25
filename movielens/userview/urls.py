b2d7835ba0aee211094e7e7ee1205b947f2e44fb
teraz_kubi <kubacichowicz13@wp.pl>
2023-05-25 03:17:08 +0200

    refactored models; basic templates

--- movielens/userview/urls.py
+++ movielens/userview/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("admin_view", views.admin, name="search"),
+    path("movie/<int:pk>", views.movie, name="movie"),
 
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
